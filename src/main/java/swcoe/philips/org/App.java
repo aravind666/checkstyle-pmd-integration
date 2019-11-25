1563b0dc8dd38ad27d8a03397da2701321c14e2d
Aravind HU <aravind.udayashankara@gmail.com>
2019-11-25 16:25:14 +0530

    Moving all POMs and resources to public repo

---
+++ src/main/java/swcoe/philips/org/App.java
@@ -0,0 +1,15 @@
+package swcoe.philips.org;
+
+import java.io.*;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
